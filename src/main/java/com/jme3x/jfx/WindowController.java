22977b634723bf10a274f979f0af96811f90245a
Kai Börnert <kai-boernert@visiongamestudios.de>
2014-02-20 20:14:17 +0100

    initial window resizing with border logic
    Signed-off-by: Kai Börnert <kai-boernert@visiongamestudios.de>

--- src/main/java/com/jme3x/jfx/WindowController.java
+++ src/main/java/com/jme3x/jfx/WindowController.java
@@ -11,25 +11,36 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 public class WindowController {
-	private double	dragX;
-	private double	dragY;
+	private final static int	borderSize	= 2;
+	private double				dragX;
+	private double				dragY;
 
 	@FXML
-	ScrollPane		contentPane;
+	ScrollPane					contentPane;
 	@FXML
-	Button			minimize;
+	Button						minimize;
 	@FXML
-	Button			maximize;
+	Button						maximize;
 	@FXML
-	Button			close;
+	Button						close;
 	@FXML
-	HBox			buttonHolder;
+	HBox						buttonHolder;
 	@FXML
-	Label			title;
+	Label						title;
 	@FXML
-	Region			windowRoot;
+	Region						windowRoot;
+
+	@FXML
+	StackPane					spane;
+	protected boolean			pressed;
+	protected EBorder			resizeDir;
+	protected double			initialSizeX;
+	protected double			initialSizeY;
+	protected double			initPosX;
+	protected double			initPosY;
 
 	@FXML
 	private void initialize() {
@@ -41,24 +52,185 @@ public class WindowController {
 				parent.getChildren().remove(WindowController.this.windowRoot);
 			}
 		});
-
+		//
 		this.setupHeaderDragMove();
+
+		this.windowRoot.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(final MouseEvent event) {
+				if (WindowController.this.resizeDir != EBorder.None) {
+					WindowController.this.pressed = true;
+					WindowController.this.dragX = event.getSceneX();
+					WindowController.this.dragY = event.getSceneY();
+					WindowController.this.initialSizeX = WindowController.this.windowRoot.getWidth();
+					WindowController.this.initialSizeY = WindowController.this.windowRoot.getHeight();
+					WindowController.this.initPosX = WindowController.this.windowRoot.getTranslateX();
+					WindowController.this.initPosY = WindowController.this.windowRoot.getTranslateY();
+					System.out.println("Starting resize " + WindowController.this.resizeDir);
+				}
+			}
+		});
+
+		this.windowRoot.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(final MouseEvent event) {
+				WindowController.this.pressed = false;
+			}
+		});
+
+		this.windowRoot.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(final MouseEvent mouseEvent) {
+				if (WindowController.this.pressed) {
+					System.out.println(mouseEvent);
+					final double difX = mouseEvent.getSceneX() - WindowController.this.dragX;
+					final double difY = mouseEvent.getSceneY() - WindowController.this.dragY;
+					System.out.println(difX + "," + difY);
+
+					switch (WindowController.this.resizeDir) {
+					case East:
+						WindowController.this.resizeEast(difX);
+						break;
+					case None:
+						break;
+					case North:
+						WindowController.this.resizeNorth(difY);
+						break;
+					case NorthEast:
+						break;
+					case NorthWest:
+						break;
+					case South:
+						WindowController.this.resizeSouth(difY);
+						break;
+					case SouthEast:
+						break;
+					case SouthWest:
+						break;
+					case West:
+						WindowController.this.resizeWest(difX);
+						break;
+					}
+				}
+			}
+		});
 
 		this.windowRoot.setOnMouseMoved(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(final MouseEvent mouseEvent) {
-				System.out.println(mouseEvent);
-				WindowController.this.buttonHolder.setCursor(Cursor.N_RESIZE);
+				WindowController.this.resizeDir = WindowController.this.determineBorder(mouseEvent);
+				switch (WindowController.this.resizeDir) {
+				case NorthWest:
+					WindowController.this.windowRoot.setCursor(Cursor.NW_RESIZE);
+					break;
+				case East:
+					WindowController.this.windowRoot.setCursor(Cursor.E_RESIZE);
+					break;
+				case None:
+					break;
+				case North:
+					WindowController.this.windowRoot.setCursor(Cursor.N_RESIZE);
+					break;
+				case NorthEast:
+					WindowController.this.windowRoot.setCursor(Cursor.NE_RESIZE);
+					break;
+				case South:
+					WindowController.this.windowRoot.setCursor(Cursor.S_RESIZE);
+					break;
+				case SouthEast:
+					WindowController.this.windowRoot.setCursor(Cursor.SE_RESIZE);
+					break;
+				case SouthWest:
+					WindowController.this.windowRoot.setCursor(Cursor.SW_RESIZE);
+					break;
+				case West:
+					WindowController.this.windowRoot.setCursor(Cursor.W_RESIZE);
+					break;
+				}
+
 			}
 		});
 
-		this.windowRoot.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(final MouseEvent mouseEvent) {
-				System.out.println(mouseEvent);
-				WindowController.this.buttonHolder.setCursor(Cursor.N_RESIZE);
+	}
+
+	protected void resizeWest(final double difX) {
+		final double newWidth = WindowController.this.initialSizeX - difX;
+		if (newWidth < WindowController.this.windowRoot.minWidthProperty().doubleValue()) {
+			return;
+		}
+		if (newWidth > WindowController.this.windowRoot.maxWidthProperty().doubleValue()) {
+			return;
+		}
+		WindowController.this.windowRoot.setTranslateX(WindowController.this.initPosX + difX);
+		WindowController.this.windowRoot.setPrefWidth(newWidth);
+	}
+
+	protected void resizeEast(final double difX) {
+		final double newWidth = WindowController.this.initialSizeX + difX;
+		if (newWidth < WindowController.this.windowRoot.minWidthProperty().doubleValue()) {
+			return;
+		}
+		if (newWidth > WindowController.this.windowRoot.maxWidthProperty().doubleValue()) {
+			return;
+		}
+		WindowController.this.windowRoot.setPrefWidth(newWidth);
+	}
+
+	protected void resizeSouth(final double difY) {
+		final double newHeight = WindowController.this.initialSizeY + difY;
+		if (newHeight < WindowController.this.windowRoot.minHeightProperty().doubleValue()) {
+			return;
+		}
+		if (newHeight > WindowController.this.windowRoot.maxHeightProperty().doubleValue()) {
+			return;
+		}
+		WindowController.this.windowRoot.setPrefHeight(newHeight);
+	}
+
+	protected void resizeNorth(final double difY) {
+		final double newHeight = WindowController.this.initialSizeY - difY;
+		if (newHeight < WindowController.this.windowRoot.minHeightProperty().doubleValue()) {
+			return;
+		}
+		if (newHeight > WindowController.this.windowRoot.maxHeightProperty().doubleValue()) {
+			return;
+		}
+		WindowController.this.windowRoot.setPrefHeight(newHeight);
+		WindowController.this.windowRoot.setTranslateY(WindowController.this.initPosY + difY);
+
+	}
+
+	private EBorder determineBorder(final MouseEvent mouseEvent) {
+		final double left = mouseEvent.getSceneX() - WindowController.this.windowRoot.getTranslateX();
+		final double right = mouseEvent.getSceneX()
+				- (WindowController.this.windowRoot.getTranslateX() + (WindowController.this.windowRoot.getWidth() - WindowController.borderSize));
+		final double up = mouseEvent.getSceneY() - WindowController.this.windowRoot.getTranslateY();
+		final double down = mouseEvent.getSceneY()
+				- (WindowController.this.windowRoot.getTranslateY() + (WindowController.this.windowRoot.getHeight() - WindowController.borderSize));
+		if (left < WindowController.borderSize && left > 0) {
+			if (up < WindowController.borderSize && up > 0) {
+				return EBorder.NorthWest;
+			} else if (down < WindowController.borderSize && down > 0) {
+				return EBorder.SouthWest;
+			} else {
+				return EBorder.West;
 			}
-		});
+		} else if (right < WindowController.borderSize && right > 0) {
+			if (up < WindowController.borderSize && up > 0) {
+				return EBorder.NorthEast;
+			} else if (down < WindowController.borderSize && down > 0) {
+				return EBorder.SouthEast;
+			} else {
+				return EBorder.East;
+			}
+		} else if (up < WindowController.borderSize && up > 0) {
+			return EBorder.North;
+		} else if (down < WindowController.borderSize && down > 0) {
+			return EBorder.South;
+		}
+		return EBorder.None;
 	}
 
 	private void setupHeaderDragMove() {
