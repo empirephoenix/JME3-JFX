cb2e24213e2df28a0c94c3d53c0529cf49973769
Kai Boernert <kai-boernert@visiongamestudios.de>
2015-01-02 12:44:01 +0100

    event fixes
    Signed-off-by: Kai Boernert <kai-boernert@visiongamestudios.de>

--- src/main/java/com/jme3x/jfx/ResizeAndMoveHelper.java
+++ src/main/java/com/jme3x/jfx/ResizeAndMoveHelper.java
@@ -84,8 +84,8 @@ public class ResizeAndMoveHelper {
 			final double mouseEventX = mouseEvent.getSceneX(), mouseEventY = mouseEvent.getSceneY(), sceneWidth = scene.getWidth(), sceneHeight = scene.getHeight();
 
 			if (MouseEvent.MOUSE_MOVED.equals(mouseEventType) == true) {
-				this.resizing = true;
 				if (mouseEventX < this.border && mouseEventY < this.border) {
+					this.resizing = true;
 					this.cursorEvent = Cursor.NW_RESIZE;
 				} else if (mouseEventX < this.border && mouseEventY > sceneHeight - this.border) {
 					this.cursorEvent = Cursor.SW_RESIZE;
@@ -102,7 +102,6 @@ public class ResizeAndMoveHelper {
 				} else if (mouseEventY > sceneHeight - this.border) {
 					this.cursorEvent = Cursor.S_RESIZE;
 				} else {
-					this.resizing = false;
 					this.cursorEvent = Cursor.DEFAULT;
 
 				}
@@ -111,16 +110,19 @@ public class ResizeAndMoveHelper {
 				this.startX = this.stage.getWidth() - mouseEventX;
 				this.startY = this.stage.getHeight() - mouseEventY;
 			} else if (MouseEvent.MOUSE_DRAGGED.equals(mouseEventType) == true) {
+				this.resizing = false;
 				if (Cursor.DEFAULT.equals(this.cursorEvent) == false) {
 					if (Cursor.W_RESIZE.equals(this.cursorEvent) == false && Cursor.E_RESIZE.equals(this.cursorEvent) == false) {
 						final double minHeight = this.stage.getMinHeight() > (this.border * 2) ? this.stage.getMinHeight() : (this.border * 2);
 						if (Cursor.NW_RESIZE.equals(this.cursorEvent) == true || Cursor.N_RESIZE.equals(this.cursorEvent) == true || Cursor.NE_RESIZE.equals(this.cursorEvent) == true) {
 							if (this.stage.getHeight() > minHeight || mouseEventY < 0) {
+								this.resizing = true;
 								this.stage.setHeight(this.stage.getY() - mouseEvent.getScreenY() + this.stage.getHeight());
 								this.stage.setY(mouseEvent.getScreenY());
 							}
 						} else {
 							if (this.stage.getHeight() > minHeight || mouseEventY + this.startY - this.stage.getHeight() > 0) {
+								this.resizing = true;
 								this.stage.setHeight(mouseEventY + this.startY);
 							}
 						}
@@ -130,11 +132,13 @@ public class ResizeAndMoveHelper {
 						final double minWidth = this.stage.getMinWidth() > (this.border * 2) ? this.stage.getMinWidth() : (this.border * 2);
 						if (Cursor.NW_RESIZE.equals(this.cursorEvent) == true || Cursor.W_RESIZE.equals(this.cursorEvent) == true || Cursor.SW_RESIZE.equals(this.cursorEvent) == true) {
 							if (this.stage.getWidth() > minWidth || mouseEventX < 0) {
+								this.resizing = true;
 								this.stage.setWidth(this.stage.getX() - mouseEvent.getScreenX() + this.stage.getWidth());
 								this.stage.setX(mouseEvent.getScreenX());
 							}
 						} else {
 							if (this.stage.getWidth() > minWidth || mouseEventX + this.startX - this.stage.getWidth() > 0) {
+								this.resizing = true;
 								this.stage.setWidth(mouseEventX + this.startX);
 							}
 						}
